5110ac0a6633f4edb44c050a0442f928c0e379a9
sheikh-momin <momin.emon14@gmail.com>
2022-11-09 04:17:02 -0800

    post services

--- index.js
+++ index.js
@@ -16,18 +16,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run (){
   try{
       const serviceCollection = client.db('cesarPhotography').collection('services')
+    const commentCollection = client.db('cesarPhotography').collection('comments')
     app.get('/services', async(req, res)=>{
         const query = {}
         const cursor =serviceCollection.find(query);
         const services = await cursor.toArray();
         res.send(services);
-    })
+    });
 
     app.get('/services/:id', async(req,res)=>{
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query)
       res.send(service);
+    });
+
+    app.post('/services', async (req, res) => {
+      const postServices = req.body;
+      const result = await serviceCollection.insertOne(postServices);
+      res.send(result);
+
     })
 
   }
